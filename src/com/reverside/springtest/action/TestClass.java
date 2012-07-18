66b1efa3cbf98f072f439e55fff069c96c68c4f1
Anusree Ramakrishnan <anusree.ramakrishnan@reverside.co.za>
2012-07-18 13:49:41 +0200

    This is a comment written by Anusree

--- src/com/reverside/springtest/action/TestClass.java
+++ src/com/reverside/springtest/action/TestClass.java
@@ -4,4 +4,8 @@ package com.reverside.springtest.action;
  * Test class for testing merge Operation
  */
 public class TestClass {
+
+    public static void main(String[] args) {
+        System.out.println("lalalalalala");
+    }
 }
