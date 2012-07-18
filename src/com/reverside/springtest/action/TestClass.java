f63cad803f024c54f02ae489d13b8201540a9cfd
Anusree Ramakrishnan <anusree.ramakrishnan@reverside.co.za>
2012-07-18 15:11:52 +0200

    Added package main Added file Denomination Changed TestClass Comment Changed comment in ListUserAction

--- src/com/reverside/springtest/action/TestClass.java
+++ src/com/reverside/springtest/action/TestClass.java
@@ -3,11 +3,12 @@ package com.reverside.springtest.action;
 /**
  * Test class for testing merge Operation
  *
- * dfdsadsadsadsSAaAAASDGFFG
+ * i deleted this comment
  */
 public class TestClass {
 
     public static void main(String[] args) {
-        System.out.println("lalala rararara");
+
+        System.out.println("lalala rararara kkkkkkka");
 }
 }
---
+++ src/com/reverside/springtest/model/Denomination.java
@@ -0,0 +1,30 @@
+package com.reverside.springtest.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Anusree
+ * Date: 2012/07/18
+ * Time: 3:02 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Denomination {
+    
+    private int id;
+    private int value;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
