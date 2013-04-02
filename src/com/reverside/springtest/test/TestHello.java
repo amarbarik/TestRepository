4e9d377c3add3454185d6bdbc44bce70478ad061
Chinmay45 <chnmay45@gmail.com>
2013-04-02 10:51:57 +0200

    Added this one 2nd April

---
+++ src/com/reverside/springtest/test/TestHello.java
@@ -0,0 +1,15 @@
+package com.reverside.springtest.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Chinmaya
+ * Date: 2013/04/02
+ * Time: 10:48 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestHello {
+
+    public static void main (String args[]){
+        System.out.println("Test Repo");
+    }
+}
